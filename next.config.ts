b816349b3cc5037a4c0e8629b5e87d9615fcc45a
Shek Rasel <swe.rasel@gmail.com>
2025-09-04 06:31:06 +0600

    config for photo url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "via.placeholder.com", // fallback avatar
+      "images.unsplash.com", // Unsplash images
+      "res.cloudinary.com",
+      "www.shutterstock.com",
+      "static.vecteezy.com",
+      "randomuser.me",
+      "http://cvbcvb",
+    ],
+  },
 };
 
 export default nextConfig;
